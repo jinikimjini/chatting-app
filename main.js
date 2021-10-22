18d43056465dedbf35d54f5786a079358ab47487
jinikimjini <jjjn0211@naver.com>
2021-10-22 23:59:18 +0900

    feat: add login function

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-var http = require('http'); //모듈 가져오는 것, 로딩결과를 담음
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./src/lib/template.js');
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var crypto = require('crypto');
+var bodyParser = require('body-parser'); //post request data의 body로 부터 파라미터를 편리하게 추출
 var path = require('path'); //경로 보안 , 오염된 정보 들어올때
 var sanitizeHtml = require('sanitize-html');//보안, 오염된 정보를 내보낼때
 var mysql = require('mysql');
-var db = mysql.createConnection({
-    host :"localhost",
-    user : "root",
-    password : "1234",
-    database : "genie"
+var dbConfig = require('./src/db/mysql.js');
+const { user } = require('./src/db/mysql.js');
+var db = {
+    host : dbConfig.host,
+    user : dbConfig.user,
+    password : dbConfig.password,
+    database : dbConfig.database
   
+};
+
+
+var conn = mysql.createConnection(db);
+conn.connect();
+
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'ejs');//화면 engine을 ejs로 설정 (기본엔진)
+app.set('views','./views'); //view 경로 설정
+
+app.get('/', function (req, res) {
+    res.render('login');
 });
-db.connect();
 
-
-var app = http.createServer(function(request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-
-
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-
-        var html = template.htmlMain( `
-        <form action="/login" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p><textarea name="description" placeholder="description"></textarea></p>
-        <p><input type="submit"/></p>
-        </form>
-        `,'');
-        response.writeHead(200); //성공적으로 전송했다.
-        response.end(html);
-     
-
-
-    } else if (pathname === '/login') {
-
-          response.writeHead(200); //성공적으로 전송했다.
-          response.end(html);
-
+app.post('/login', function(req, res){
+  var id= req.body.memId;
+  var pwd = req.body.memPwd;
+  var sql = 'SELECT * FROM member WHERE memId=?';
+  conn.query(sql, [id], function(err, results){
+    if(err){
+      console.log(err);
     }
+    if(!results[0]){
+      return res.send('please check your id.');
+    }
+    
+    var member = results[0];
+      if(pwd === member.memPwd){
+        return res.send('login success');
+      } else {
+        return res.send('please check your password')
+      }
 
-  } 
+  })
+})
 
-   else {
-    response.writeHead(404); // 페이지를 찾을 수 없다.
-    response.end('Not found');
-  }
-
-
-});
 app.listen(5000); //서버를 구동시키는 api
--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,18 +1,20 @@
-var mysql = require('mysql');
+/*var crypto = require('crypto');
+var salt = '';
+var pwd = '';
+crypto.randomBytes(64, (err, buf) =>{
+    if(err) throw err;
+    salt = buf.toString('hex');
+});
+crypto.pbkdf2('password', salt, 100000, 64, 'sha512', (err, derivedKey)=>{
+ if(err) throw err;
+ pwd = derivedKey.toString('hex');   
+});*/
 
-var connection = mysql.createConncetion({
+var dbConfig = {
     host :"localhost",
     user : "root",
     password : "1234",
     database : "genie"
-});
+};
 
-connection.connect("select * from genie", function (error, results){
-    if(error){
-        console.log(error);
-
-    }
-    console.log(results);
-});
-
-connection.end();
+module.exports = dbConfig;
